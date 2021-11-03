fd817225cdb5fc72acdfa1a62cb86101caedd85f
ansh sachdeva <ansh.sachdeva@skuad.io>
2021-11-03 22:57:07 +0530

    [core-jdk] grammar fixes

--- core_jdk/src/main/java/work/curioustools/core_jdk/StringExtenstions.kt
+++ core_jdk/src/main/java/work/curioustools/core_jdk/StringExtenstions.kt
@@ -109,18 +109,18 @@ object CommonStringRegex {
 
 enum class EmailValidationResponse(val msg: String) {
     VALID("Success"),
-    NULL_OR_BLANK("Must not be empty"),
-    IMPROPER_EMAIL("email does not match the standard email pattern of ${CommonStringRegex.VALID_EMAIL}")
+    NULL_OR_BLANK("Email must not be empty"),
+    IMPROPER_EMAIL("Email does not match the standard email pattern of ${CommonStringRegex.VALID_EMAIL}")
 }
 
 sealed class PasswordValidationResponse(open val msg: String) {
     object VALID : PasswordValidationResponse("Success")
-    object NULL_OR_BLANK : PasswordValidationResponse("Must not be empty")
-    data class NO_MIN_UPPERCASE_LETTERS(val minCount: Int = 1) : PasswordValidationResponse("Must have at least $minCount Capital letter(s)")
-    data class NO_LOWERCASE_LETTERS(val minCount: Int = 1) : PasswordValidationResponse("Must have at least $minCount Lowercase letter(s)")
-    data class NO_MIN_Special_LETTERS(val minCount: Int = 1) : PasswordValidationResponse("Must have at least $minCount letter(s) from ${CommonStringRegex.VALID_SPECIAL_CHARACTERS}")
-    data class NO_MIN_DIGITS(val minCount: Int = 1) : PasswordValidationResponse("Must have at least $minCount digit(s)")
-    data class OUT_OF_BOUNDS_ERROR(val min: Int, val max: Int) : PasswordValidationResponse("Must have $min to $max characters")
+    object NULL_OR_BLANK : PasswordValidationResponse("Password must not be empty")
+    data class NO_MIN_UPPERCASE_LETTERS(val minCount: Int = 1) : PasswordValidationResponse("Password must have at least $minCount Capital letter(s)")
+    data class NO_LOWERCASE_LETTERS(val minCount: Int = 1) : PasswordValidationResponse("Password must have at least $minCount Lowercase letter(s)")
+    data class NO_MIN_Special_LETTERS(val minCount: Int = 1) : PasswordValidationResponse("Password must have at least $minCount letter(s) from ${CommonStringRegex.VALID_SPECIAL_CHARACTERS}")
+    data class NO_MIN_DIGITS(val minCount: Int = 1) : PasswordValidationResponse("Password must have at least $minCount digit(s)")
+    data class OUT_OF_BOUNDS_ERROR(val min: Int, val max: Int) : PasswordValidationResponse("Password must have $min to $max characters")
     data class ILLEGAL_CHARACTER(val c: Char) : PasswordValidationResponse(" '$c ' is not a valid character")
 
 
